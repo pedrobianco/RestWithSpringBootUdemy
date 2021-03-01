2356c9456de01957af2acd347a258381e24a7a09
Pedro Bianco <pedroalasticobianco@gmail.com>
2021-03-01 19:40:37 -0300

    Verbs HTTP

---
+++ 03 RestWithSpringBootUdemy Verbs HTTP/src/main/java/br/com/pedro/PersonController.java
@@ -0,0 +1,56 @@
+package br.com.pedro;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pedro.model.Person;
+import br.com.pedro.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonService services;
+	
+	@RequestMapping(value="/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable("id") String id) {
+		return services.findById(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return services.findAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return services.create(person);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return services.update(person);
+	}
+	
+	@RequestMapping(value="/{id}",
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) {
+		services.delete(id);
+	}
+	
+}
---
+++ 03 RestWithSpringBootUdemy Verbs HTTP/src/main/java/br/com/pedro/services/PersonService.java
@@ -0,0 +1,59 @@
+package br.com.pedro.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.pedro.model.Person;
+
+@Service
+public class PersonService {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}	
+	
+	public void delete(String id) {
+	}
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Pedro");
+		person.setLastName("Bianco");
+		person.setAddress("Ribeirao Preto - São Paulo -Brasil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 7; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAddress("Some address in Brasil" + i);
+		person.setGender("Male");
+		return person;
+	}
+	
+	
+	
+	
+}
